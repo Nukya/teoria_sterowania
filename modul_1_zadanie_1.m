f457d8634032dbeb377d5b64aa4af8ee0f8facae
Nukya <53628577+Nukya@users.noreply.github.com>
2025-10-07 19:25:15 +0200

    zad 1 i 2

---
+++ modul_1_zadanie_1.m
@@ -0,0 +1,28 @@
+clear
+close all
+clc
+nfontslatex = 18;
+nfonts = 14;
+m = 1.0;
+f = @(t,x) [x(2); m*(1-x(1)^2)*x(2)-x(1)];
+tInit = 0.0;
+tFinal = 20.0;
+xInit = [2; 0];
+options = odeset('RelTol',1e-14,'AbsTol',1e-14);
+[t,X] = ode45(f,[tInit,tFinal],xInit,options);
+figure
+plot(t,X(:,1),'r','LineWidth',2.0)
+hold on
+grid on
+plot(t,X(:,2),'b','LineWidth',2.0)
+set(gca,'FontSize',nfonts)
+xlabel('$t$','Interpreter','Latex', ...
+'FontSize',nfontslatex)
+ylabel('$x_{1},\,x_{2}$', ...
+'Interpreter','Latex','FontSize',nfontslatex)
+legend({'$x_{1}$','$x_{2}$'}, ....
+'Interpreter','Latex', ...
+'FontSize',nfontslatex,'Location','Best')
+print('VDP.eps','-depsc','-r600')
+print('VDP.jpg','-djpeg','-r600')
+print('VDP.pdf','-dpdf','-r600')
