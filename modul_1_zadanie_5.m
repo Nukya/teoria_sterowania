0c7a0c9a3c483988ee20711dccd90e1937a34688
Nukya <53628577+Nukya@users.noreply.github.com>
2025-10-19 20:10:17 +0200

    poprawione

--- modul_1_zadanie_5.m
+++ modul_1_zadanie_5.m
@@ -2,21 +2,29 @@ clear;
 clc;
 close all;
 
-% Parametry układu 
-L = 180e-6;     % [H]
-C = 5.4e-6;     % [F]
-R = 9;          % [Ohm]
-Vin = 15;       % [V]
-fs = 40e3;      % [Hz]
+% Surowe dane
+
+% L = 180uH
+% C = 5.4 uF
+% R = 9 Ohm
+% Vi = 15 V 
+% fs = 40 kHz
+
+% Dane w podstawowych jednostkach SI
+L = 180e-6;     % [H]           180uH
+C = 5.4e-6;     % [F]           5.4 uF
+R = 9;          % [Ohm]         9 Ohm
+Vi = 15;        % [V]           15 V 
+fs = 40e3;      % [Hz]          40 kHz
 ws = 2*pi*fs;   % [rad/s]
 
-% Czas symulacji 
+% Rozpatrywany czas
 tspan = [0 3e-3]; % [s]
 
-% Funkcja przełączająca 
-u = @(t) 0.5*(1 - sign(sin(ws*t)));
+% Funkcja przełączania
+u = @(t) (1-sign(sin(ws*t)))/2;
 
-% Uproszczone macierze stanu zakładając R1=R2=R3=0
+% Uproszczone macierze stanu dla R1=R2=R3=0
 A = [0      -1/L;
     1/C -1/(R*C)];
 
@@ -25,7 +33,7 @@ B = [1/L;
 
 b = [0; 0];
 
-f = [0; Vin/(C*R)];
+f = [0; Vi/(C*R)];
 
 % Równanie stanu 
 dxdt = @(t, x) (A*x + B*u(t) + f);
@@ -33,14 +41,11 @@ dxdt = @(t, x) (A*x + B*u(t) + f);
 % Warunki początkowe 
 x0 = [0; 0]; % [i_l, v_C]
 
-% Symulacja 
 [t, x] = ode45(dxdt, tspan, x0);
-
 
 iL = x(:, 1);
 vC = x(:, 2); 
 
-% Sekcja kreślenia wykresu 
 figure;
 subplot(2,1,1);
 plot(t, iL, 'LineWidth', 1.2);
--- modul_1_zadanie_6.m
+++ modul_1_zadanie_6.m
@@ -1,60 +1,65 @@
-% Zadanie 6 – portret fazowy układu van der Pol
-clear; 
-close all; 
-clc;
+clear
+close all
+clc
 
-% Parametr układu
-m = 1.0;
+nfontslatex = 18;
+nfonts = 14;
+m = 1.0;   % parametr nieliniowości
 
-% Definicja równań van der Pola
-f = @(t, x) [ x(2);
-              m*(1 - x(1)^2)*x(2) - x(1) ];
+% Definicja równania różniczkowego
+f = @(t,x) [x(2); m*(1 - x(1)^2)*x(2) - x(1)];
 
-% Siatka dla pola wektorowego
-x1_min = -4;  x1_max = 4;
-x2_min = -4;  x2_max = 4;
-stepSize = 0.4;
+% Funkcje składowe dla pola wektorowego
+f1 = @(x1,x2) x2;
+f2 = @(x1,x2) m*(1 - x1.^2).*x2 - x1;
 
-[x1, x2] = meshgrid(x1_min:stepSize:x1_max, x2_min:stepSize:x2_max);
+% Zakres i siatka
+x1_min = -4.0;
+x1_max = 4.0;
+x2_min = -4.0;
+x2_max = 4.0;
+stepSize = 0.3;
 
-u = x2;
-v = m*(1 - x1.^2).*x2 - x1;
+[X1,X2] = meshgrid(x1_min:stepSize:x1_max, x2_min:stepSize:x2_max);
+Y1 = arrayfun(f1,X1,X2);
+Y2 = arrayfun(f2,X1,X2);
 
-% Normalizacja wektorów dla czytelności
-n = sqrt(u.^2 + v.^2);
-u_n = u ./ n;
-v_n = v ./ n;
+% Normalizacja wektorów (na potrzeby quivera)
+L = sqrt(Y1.^2 + Y2.^2);
+Y1_n = Y1 ./ L;
+Y2_n = Y2 ./ L;
 
-% Rysowanie pola wektorowego (portret fazowy)
-figure;
-quiver(x1, x2, u_n, v_n, 0.5, 'k');
-xlabel('x_1');
-ylabel('x_2');
-title('Portret fazowy dla układu van der Pola');
-axis equal; grid on; hold on;
+% Rysunek pola wektorowego
+figure
+quiver(X1,X2,Y1_n,Y2_n,'b')
+daspect([1 1 1])
+hold on
+xlabel('$x_1$','Interpreter','latex','FontSize',nfontslatex)
+ylabel('$x_2$','Interpreter','latex','FontSize',nfontslatex)
+title('Trajektorie równania Van der Pola','Interpreter','latex','FontSize',nfontslatex)
 
-% Integracja dla różnych warunków początkowych
-tFinal = 40;
-options = odeset('RelTol',1e-9,'AbsTol',1e-12);
+tInit = 0.0;
+tFinal = 20.0;
+options = odeset('RelTol',1e-12,'AbsTol',1e-12);
 
-% Lista warunków początkowych
-x0_list = [
-    2.0, 0.0;   % Cykl graniczny 
-    0.5, 0.0;
-   -2.0, 0.0;
-    1.5, 1.0;
-];
+% kilka różnych warunków początkowych
+xInits = [  2   0;
+           -2   0;
+            0   2;
+            0  -2;
+            1   1;
+           -1  -1];
 
-for i = 1:size(x0_list,1)
-    x0 = x0_list(i,:)';
-    [t, x] = ode45(f, [0 tFinal], x0, options);
+colors = lines(size(xInits,1));
 
-    if all(abs(x0 - [2;0]) < 1e-10)
-        plot(x(:,1), x(:,2), 'b', 'LineWidth', 2.0);
-    else
-        plot(x(:,1), x(:,2), 'r', 'LineWidth', 1.0);
-    end
+for i = 1:size(xInits,1)
+    xInit = xInits(i,:)';
+    [t,X] = ode45(f,[tInit,tFinal],xInit,options);
+    plot(X(:,1),X(:,2),'Color',colors(i,:),'LineWidth',2.0)
 end
 
-legend('Pole wektorowe', 'Trajektorie', 'Location', 'best');
-axis tight;
+axis tight
+legend(arrayfun(@(i) sprintf('x_0 = [%g, %g]', xInits(i,1), xInits(i,2)), 1:size(xInits,1), 'UniformOutput', false), ...
+       'Location','bestoutside')
+grid on
+hold off
